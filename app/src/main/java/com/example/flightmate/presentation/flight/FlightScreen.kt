1967ecc9f15048e6ae4acaa5dadad33e6ca92bd9
River <shinshigobi@gmail.com>
2025-07-02 06:51:47 +0800

    feat: add top app bar to FlightScreen
    - Wrapped FlightScreen content in a Scaffold and added a top app bar labeled "航班資訊" for better structure and navigation context.
    - Preserved existing flight list display and layout within the scaffold.

--- app/src/main/java/com/example/flightmate/presentation/flight/FlightScreen.kt
+++ app/src/main/java/com/example/flightmate/presentation/flight/FlightScreen.kt
@@ -5,11 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.flightmate.presentation.flight.component.FlightCard
@@ -20,18 +26,33 @@ fun FlightScreen(
 ) {
     val flightList = viewModel.flightList
 
-    Column(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.background)
-            .fillMaxSize()
-    ) {
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+    Scaffold(
+        topBar = { FlightTopBar() }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .background(MaterialTheme.colorScheme.background)
+                .fillMaxSize()
         ) {
-            items(flightList) { flight ->
-                FlightCard(flight = flight)
+            // TODO 篩選列
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(flightList) { flight ->
+                    FlightCard(flight = flight)
+                }
             }
         }
     }
+}
+
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlightTopBar() {
+    CenterAlignedTopAppBar(
+        title = { Text("航班資訊") }
+    )
 }
