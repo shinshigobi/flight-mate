468ad9fbeedcf57b8ef342408f7eadadb11ec69a
River <shinshigobi@gmail.com>
2025-07-03 17:59:15 +0800

    feat: implement CurrencyScreen with UI and ViewModel logic
    - Built CurrencyScreen UI using Scaffold, displaying converted currencies in a LazyColumn via CurrencyCard.
    - Developed CurrencyViewModel to manage exchange rate data and conversion logic using StateFlow and combine.
    - Automatically fetches currency data on init and provides formatted conversion results based on base currency and input amount.

--- app/src/main/java/com/example/flightmate/presentation/currency/CurrencyScreen.kt
+++ app/src/main/java/com/example/flightmate/presentation/currency/CurrencyScreen.kt
@@ -1,6 +1,63 @@
 package com.example.flightmate.presentation.currency
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.flightmate.presentation.currency.component.CurrencyCard
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CurrencyScreen() {}
+fun CurrencyScreen(
+    viewModel: CurrencyViewModel = hiltViewModel()
+) {
+    val inputState by viewModel.inputState.collectAsState()
+    val currencyUiModel by viewModel.currencyUiModel.collectAsState()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("幣別換算") }
+            )
+        }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxHeight()
+                .padding(padding)
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.background)
+                    .fillMaxSize()
+            ) {
+                LazyColumn(
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(currencyUiModel.convertedList) { convertedCurrency ->
+                        CurrencyCard(currency = convertedCurrency)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/flightmate/presentation/currency/CurrencyViewModel.kt
@@ -0,0 +1,79 @@
+package com.example.flightmate.presentation.currency
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flightmate.domain.model.currency.ConvertedCurrency
+import com.example.flightmate.domain.model.currency.CurrencyInputState
+import com.example.flightmate.domain.usecase.currency.GetExchangeRateUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyViewModel @Inject constructor(
+    val getCurrencyDataUseCase: GetExchangeRateUseCase
+): ViewModel() {
+    private val _exchangeRateMap = MutableStateFlow<Map<String, Double>>(emptyMap())
+
+    private val _inputState = MutableStateFlow<CurrencyInputState>(CurrencyInputState())
+    val inputState = _inputState.asStateFlow()
+
+    val currencyUiModel = combine(
+        _exchangeRateMap, _inputState
+    ) { exchangeRateMap, inputState ->
+        val list = mapToConvertedList(exchangeRateMap, inputState)
+        CurrencyUiModel(inputState, list)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = CurrencyUiModel(CurrencyInputState(), emptyList())
+    )
+
+    init {
+        loadCurrencies()
+    }
+
+    fun loadCurrencies() {
+        viewModelScope.launch {
+            val result = getCurrencyDataUseCase.invoke(
+                "fca_live_dSqZrcLeMaOu0fAT6mktIAgt136ymgnjd8nLifxv",
+                null,
+                null
+            )
+            result.onSuccess { exchangeRateMap ->
+                _exchangeRateMap.value = exchangeRateMap
+            }.onFailure {
+                // TODO: Handle error
+            }
+        }
+    }
+
+    fun changeBaseCurrency(currencyCode: String) {
+        _inputState.value = _inputState.value.copy(baseCurrency = currencyCode)
+        // TODO fetch api
+    }
+
+    fun updateAmount(amount: String) {
+        _inputState.value = _inputState.value.copy(inputAmount = amount.toDouble())
+    }
+
+    fun mapToConvertedList(
+        rateMap: Map<String, Double>,
+        input: CurrencyInputState
+    ): List<ConvertedCurrency> {
+        return rateMap
+            .filterKeys { it != input.baseCurrency }
+            .map { (currencyCode, rate) ->
+                ConvertedCurrency(
+                    code = currencyCode,
+                    exchangeRate = rate,
+                    convertedAmount = input.inputAmount * rate
+                )
+            }
+    }
+}
