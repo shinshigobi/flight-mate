c66c542cc7f8093e0129ff36afea320125db0d30
River <shinshigobi@gmail.com>
2025-07-04 14:08:50 +0800

    feat: handle UI state in CurrencyScreen with loading and error views
    - Integrated CurrencyUiState into CurrencyScreen to conditionally render loading indicator, success UI, or appropriate error content.
    - Added CurrencyErrorContent composable to display errors using predefined UI components based on exception type.

--- app/src/main/java/com/example/flightmate/presentation/currency/CurrencyScreen.kt
+++ app/src/main/java/com/example/flightmate/presentation/currency/CurrencyScreen.kt
@@ -1,26 +1,30 @@
 package com.example.flightmate.presentation.currency
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.flightmate.domain.exception.AppException
+import com.example.flightmate.presentation.common.component.ApiErrorContent
+import com.example.flightmate.presentation.common.component.NetworkErrorContent
+import com.example.flightmate.presentation.common.component.UnknownErrorContent
 import com.example.flightmate.presentation.currency.component.CurrencyCard
 import com.example.flightmate.presentation.currency.component.CurrencyInput
 
@@ -32,6 +36,7 @@ fun CurrencyScreen(
     val inputState by viewModel.inputState.collectAsState()
     val currencyUiModel by viewModel.currencyUiModel.collectAsState()
     val currencyList by viewModel.currencyList.collectAsState()
+    val uiState by viewModel.uiState.collectAsState()
 
     Scaffold(
         topBar = {
@@ -40,41 +45,71 @@ fun CurrencyScreen(
             )
         }
     ) { padding ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .fillMaxHeight()
                 .padding(padding)
         ) {
-            Box(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                CurrencyInput(
-                    state = inputState,
-                    currencyList = currencyList,
-                    onValueChange = { amountText ->
-                        viewModel.updateAmount(amountText)
-                    },
-                    onCurrencyChange = { currency ->
-                        viewModel.updateBaseCurrency(currency)
-                    }
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.background)
-                    .fillMaxSize()
-            ) {
-                LazyColumn(
-                    contentPadding = PaddingValues(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
+            if (uiState is CurrencyUiState.Error) {
+                CurrencyErrorContent(uiState) {
+                    viewModel.loadCurrencies()
+                }
+            } else {
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize()
                 ) {
-                    items(currencyUiModel.convertedList) { convertedCurrency ->
-                        CurrencyCard(currency = convertedCurrency)
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        CurrencyInput(
+                            state = inputState,
+                            currencyList = currencyList,
+                            onValueChange = { amountText ->
+                                viewModel.updateAmount(amountText)
+                            },
+                            onCurrencyChange = { currency ->
+                                viewModel.updateBaseCurrency(currency)
+                            }
+                        )
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            items(currencyUiModel.convertedList) { convertedCurrency ->
+                                CurrencyCard(currency = convertedCurrency)
+                            }
+                        }
+                    }
+                    if (uiState is CurrencyUiState.Loading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .width(48.dp)
+                                .align(Alignment.Center)
+                        )
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CurrencyErrorContent(uiState: CurrencyUiState, action: () -> Unit) {
+    val error = (uiState as CurrencyUiState.Error).error
+    when (error) {
+        is AppException.ApiError,
+        is AppException.HttpError -> {
+            ApiErrorContent(action)
+        }
+
+        is AppException.NetworkError -> {
+            NetworkErrorContent(action)
+        }
+
+        is AppException.UnknownError -> {
+            UnknownErrorContent(action)
         }
     }
 }
